7dbd26b56609b5fa9b3cb9d5e4b66c9767b89a0a
Julian Nymark <julian.nymark@nav.no>
2026-02-06 14:06:13 +0100

    do not set local hardcoded as fallback

--- next.config.js
+++ next.config.js
@@ -9,16 +9,15 @@ const nextConfig = {
   // Configure environment variables
   env: {
     // Server-side environment variables
-    APP_ENV: process.env.APP_ENV || "local",
-    VITE_APP_ENV: process.env.VITE_APP_ENV || "local",
+    APP_ENV: process.env.APP_ENV,
+    VITE_APP_ENV: process.env.VITE_APP_ENV,
     NAIS_CLUSTER_NAME: process.env.NAIS_CLUSTER_NAME,
     NAIS_NAMESPACE: process.env.NAIS_NAMESPACE,
     BACKEND_APP_NAME: process.env.BACKEND_APP_NAME,
     VITE_WONDERWALL_INGRESS: process.env.VITE_WONDERWALL_INGRESS,
 
     // Client-side environment variables (must have NEXT_PUBLIC_ prefix)
-    NEXT_PUBLIC_APP_ENV:
-      process.env.APP_ENV || process.env.VITE_APP_ENV || "local",
+    NEXT_PUBLIC_APP_ENV: process.env.APP_ENV || process.env.VITE_APP_ENV,
     NEXT_PUBLIC_WONDERWALL_INGRESS: process.env.VITE_WONDERWALL_INGRESS,
   },
 
